6a8445d455a08963f54be7a69336690938819f28
dydrich <cravenroad17@gmail.com>
2019-02-09 09:37:37 +0100

    resi dinamici nome e versione software

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <header id="header">
 	<div id="sc_firstrow">
 		<i class="material-icons" style="font-size: 1.6em">local_library</i>
-		<span style="position: relative; bottom: 8px; margin-left: 5px">e-Library 0.1</span>
+		<span style="position: relative; bottom: 8px; margin-left: 5px"><?php echo $_SESSION['__config__']['software_name'].' '.$_SESSION['__config__']['software_version'] ?></span>
 		<span style="position: relative; bottom: 8px; font-size: 0.9em; font-weight: normal" id="soft_desc"> - Software di gestione biblioteca scolastica</span>
 	</div>
 	<div id="sc_secondrow">
         <i class="material-icons" style="position: relative; top: 1px">school</i>
         <span style="position: relative; margin-left: 5px; bottom: 5px">
-            <a href="https://www.istitutoiglesiasserraperdosa.gov.it">
+            <a href="https://www.icnivolaiglesias.edu.it">
                 Istituto comprensivo Nivola, Iglesias
             </a>
         </span>
