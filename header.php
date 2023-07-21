94ebb32a58d79a6f6ef4907c64de696356e78d75
Riccardo Bachis <cravenroad17@gmail.com>
2023-07-21 12:42:59 +0200

    Sistemazione grafica home page

--- header.php
+++ header.php
@@ -1,15 +1,34 @@
-<header id="header">
+<header id="header" style="background-image: url('../images/library3.png')">
 	<div id="sc_firstrow">
 		<i class="material-icons" style="font-size: 1.6em">local_library</i>
 		<span style="position: relative; bottom: 8px; margin-left: 5px"><?php echo $_SESSION['__config__']['software_name'].' '.$_SESSION['__config__']['software_version'] ?></span>
-		<span style="position: relative; bottom: 8px; font-size: 0.9em; font-weight: normal" id="soft_desc"> - Software di gestione biblioteca scolastica</span>
+		<span style="position: relative; bottom: 8px; font-size: 0.9em; " id="soft_desc"></span>
 	</div>
-	<div id="sc_secondrow">
-        <i class="material-icons" style="position: relative; top: 1px">school</i>
-        <span style="position: relative; margin-left: 5px; bottom: 5px">
-            <a href="https://www.icnivolaiglesias.edu.it">
-                Istituto comprensivo Nivola, Iglesias
+	<div id="sc_secondrow" style="display: flex; justify-content: right">
+        <div style="" class="main_menu_item">
+            <a href="#">
+                <i class="material-icons">person</i>
             </a>
-        </span>
+        </div>
+        <div style="" class="main_menu_item">
+            <a href="admin/index.php">
+                <i class="material-icons">settings</i>
+            </a>
+        </div>
+        <div style="" class="main_menu_item">
+            <a href="admin/index.php">
+                <i class="material-icons">book</i>
+            </a>
+        </div>
+        <div style="" class="main_menu_item">
+            <a href="do_logout.php">
+                <i class="material-icons">cloud_off</i>
+            </a>
+        </div>
 	</div>
+    <nav id="navigation">
+        <div id="head_label" style="text-align: center">
+            <p style="font-size: 1.8em; margin-top: 15px">Biblioteca scolastica</p>
+        </div>
+    </nav>
 </header>
--- nav.php
+++ nav.php
@@ -1,8 +1,5 @@
 <nav id="navigation">
-	<div id="head_label" style="text-align: center">
-        <img alt="Stemma del Nivola" src="../images/madre.png" />
-        <p style="font-size: 1.5em">Istituto comprensivo C. Nivola, Iglesias</p>
-        <p>Scuola secondaria di primo grado</p>
-        <p style="font-size: 1.8em; margin-top: 15px">Biblioteca scolastica</p>
-    </div>
-</nav>
+        <div id="head_label" style="text-align: center">
+            <p style="font-size: 1.8em; margin-top: 15px">Biblioteca scolastica</p>
+        </div>
+    </nav>
