6db639413bbcfc3ed056921ecf8b1b2cdc402b4f
Riccardo Bachis <cravenroad17@gmail.com>
2023-08-03 18:48:49 +0200

    getter e setter per Venue

--- lib/Venue.php
+++ lib/Venue.php
@@ -13,4 +13,60 @@ class Venue
     private $id;
     private $name;
     private $code;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Venue
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return Venue
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     * @return Venue
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+
 }
