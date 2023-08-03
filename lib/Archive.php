fe6d7a09d9f9aa6dc3a0692eec4d7fb251975e91
Riccardo Bachis <cravenroad17@gmail.com>
2023-08-03 18:40:59 +0200

    nuove classi per gestione locali

---
+++ lib/Archive.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by VS Code
+ * User: riccardo
+ * Date: 03/08/23
+ * Time: 15:20
+ */
+
+namespace elibrary;
+
+class Archive
+{
+    private $venues;
+    private $rooms;
+    private $bookcases;
+
+
+}
---
+++ lib/Venue.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by VS Code
+ * User: riccardo
+ * Date: 03/08/23
+ * Time: 18:28
+ */
+
+namespace elibrary;
+
+class Venue 
+{
+    private $id;
+    private $name;
+    private $code;
+}
