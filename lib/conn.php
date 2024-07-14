d8436b36e32a8abe8fc4cb42946979095ab6198d
Riccardo Bachis <cravenroad17@gmail.com>
2024-07-14 11:11:46 +0200

    bug fixes

--- lib/conn.php
+++ lib/conn.php
@@ -1,10 +1,6 @@
 <?php
 
-// local copy
-//$db = new MySQLConnection("localhost", "root", "isildur", "edocs", "3306");
-//$db->set_charset("utf8");
-
 // rbachis.net copy
-$db = new MySQLConnection("localhost", "rbachisn_elibrary", "gOlconda666", "rbachisn_elibrary", "3306");
+$db = new MySQLConnection("localhost", "rbachisn_elibrary", "zCRi99Yr4l00", "rbachisn_e-library", "3306");
 $db->set_charset("utf8");
 //print "Connection ok";
